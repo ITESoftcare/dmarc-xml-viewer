d87c9c40c6dfd72e87eb75c0c86b954ce2a6acf6
ITESaurabh <itesaurabh@yahoo.com>
2023-02-12 20:49:42 +0530

    gh patch 2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dmarc-xml-viewer/',
+  base: '/',
   logLevel: 'error',
   plugins: [
     react(),
