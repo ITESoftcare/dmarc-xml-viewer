7ee1997ae17d23053a0a0a5f3dde957a3ecf4424
ITESaurabh <itesaurabh@yahoo.com>
2023-02-13 01:03:20 +0530

    patch try 7

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './dmarc-xml-viewer/',
+  base: '/dmarc-xml-viewer/',
   logLevel: 'error',
   plugins: [
     react(),
