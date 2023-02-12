398880dd5ab7009cb91cd10f6a0d06279983f9b4
ITESaurabh <50075159+ITESaurabh@users.noreply.github.com>
2023-02-12 19:34:18 +0000

    Deploying to gh-pages from @ ITESoftcare/dmarc-xml-viewer@7ee1997ae17d23053a0a0a5f3dde957a3ecf4424 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dmarc-xml-viewer/sw.js', { scope: '/dmarc-xml-viewer/' })})}
